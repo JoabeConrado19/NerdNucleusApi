9c7b67bd9a3a8edb331b610cf5075be514b044b7
JoabeConrado19 <joabe.conrado19@gmail.com>
2023-05-20 11:21:35 -0400

    fix get Order

--- anime/views.py
+++ anime/views.py
@@ -24,6 +24,7 @@ from .serializer import AnimeSerializer
 class AnimeView(ListCreateAPIView, PageNumberPagination):
     queryset = Anime.objects.all()
     serializer_class = AnimeSerializer
+    ordering = ['-id']
 
 
 
